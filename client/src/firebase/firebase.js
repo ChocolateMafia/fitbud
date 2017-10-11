74b9c2106c5dafa6608fdb84acd4a2ffaa70a5d1
Kenny Cao <kennyxcao@gmail.com>
2017-10-10 17:19:18 -0700

    Add firebase config file

---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: 'AIzaSyDUEf-0MaqRdvEBohGLToDACD9eDul3lvc',
+  authDomain: 'fitbud-72ff9.firebaseapp.com',
+  databaseURL: 'https://fitbud-72ff9.firebaseio.com',
+  projectId: 'fitbud-72ff9',
+  storageBucket: 'fitbud-72ff9.appspot.com',
+  messagingSenderId: '990274992056'
+};
+
+firebase.initializeApp(config);
+
+export const ref = firebase.database().ref();
+export const auth = firebase.auth;
+export const provider = new firebase.auth.FacebookAuthProvider();
