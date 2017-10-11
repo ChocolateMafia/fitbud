07751f99c31da7ea67b0a3880d74ae4118df5a11
Kenny Cao <kennyxcao@gmail.com>
2017-10-10 22:27:46 -0700

    Comment out firebase api data

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -1,12 +1,20 @@
 import firebase from 'firebase';
 
+// var config = {
+//   apiKey: 'AIzaSyDUEf-0MaqRdvEBohGLToDACD9eDul3lvc',
+//   authDomain: 'fitbud-72ff9.firebaseapp.com',
+//   databaseURL: 'https://fitbud-72ff9.firebaseio.com',
+//   projectId: 'fitbud-72ff9',
+//   storageBucket: 'fitbud-72ff9.appspot.com',
+//   messagingSenderId: '990274992056'
+// };
+
 var config = {
-  apiKey: 'AIzaSyDUEf-0MaqRdvEBohGLToDACD9eDul3lvc',
-  authDomain: 'fitbud-72ff9.firebaseapp.com',
-  databaseURL: 'https://fitbud-72ff9.firebaseio.com',
-  projectId: 'fitbud-72ff9',
-  storageBucket: 'fitbud-72ff9.appspot.com',
-  messagingSenderId: '990274992056'
+  apiKey: 'YOUR API KEY',
+  authDomain: 'YOUR AUTH DOMAIN',
+  databaseURL: 'YOUR DATABASE URL',
+  storageBucket: 'YOUR STORAGE BUCKET',
+  messagingSenderId: 'YOUR MESSAGING SENDER ID'
 };
 
 firebase.initializeApp(config);
