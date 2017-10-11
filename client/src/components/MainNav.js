349f4f61ec693333915c850e4746d1b642c52c32
Elena Bartosh <elena.karnaukhova@gmail.com>
2017-10-11 14:02:24 -0700

    Fix console warning  with listings unique key

--- client/src/components/MainNav.js
+++ client/src/components/MainNav.js
@@ -24,25 +24,25 @@ class MainNav extends Component {
           </Menu.Item>
 
           {!this.props.isAuthed && ([
-            <Menu.Item style={{paddingLeft: '0px'}}> 
+            <Menu.Item key='login' style={{paddingLeft: '0px'}}> 
               {/*<Button content='Log In' onClick={this.handleLoginClick} /> */}
               {/*<LoginButtonModal authenticate={this.props.authenticate}/> */}
               <Button as={Link} to='/login' content='Log In' />
             </Menu.Item>,
-            <Menu.Item style={{paddingLeft: '0px'}}> 
+            <Menu.Item key='signup' style={{paddingLeft: '0px'}}> 
               <Button as={Link} to='/signup' primary content='Sign Up' />
             </Menu.Item>
           ])}
 
           {this.props.isAuthed && ([
-            <Menu.Item style={{paddingLeft: '0px'}}> 
+            <Menu.Item key='create' style={{paddingLeft: '0px'}}> 
               <Button as={Link} to='/create' primary content='Create Listing' />
             </Menu.Item>,
-            <Dropdown text={this.props.user.name} className='link item' pointing>          
+            <Dropdown key='menu' text={this.props.user.name} className='link item' pointing>          
               <Dropdown.Menu> 
-                <Dropdown.Item as={Link} to='/dashboard'>Dashboard</Dropdown.Item>
-                <Dropdown.Item>Profile</Dropdown.Item>
-                <Dropdown.Item>Referral</Dropdown.Item>
+                <Dropdown.Item key='dashboard' as={Link} to='/dashboard'>Dashboard</Dropdown.Item>
+                <Dropdown.Item key='profile'>Profile</Dropdown.Item>
+                <Dropdown.Item key='referral'>Referral</Dropdown.Item>
                 <Dropdown.Divider />
                 <Dropdown.Item onClick={this.props.signoff} as={Link} to='/'>Log Out</Dropdown.Item>
               </Dropdown.Menu>
