d391a52367638ec75ccb078fefea75cd738d4599
Aileen Fan <aileen.z.fan@gmail.com>
2017-10-12 14:01:04 -0700

    rendering the posting in the Dashboard

--- client/src/components/Workouts.js
+++ client/src/components/Workouts.js
@@ -1,30 +1,68 @@
 import React, { Component } from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
-
+import { Card, Icon, Image, Transition, Container } from 'semantic-ui-react';
 import WorkoutDropdown from './WorkoutDropdown';
+import ListingModal from './ListingModal.js';
 
 class Workouts extends Component {
   constructor(props) {
     super(props);
+     this.state = {
+      visible: false,
+      listing: [],
+      showModal: false,
+      selectedListing: null
+    };
   }
 
+  componentDidMount() {
+    this.setState({visible: true});
+  }
+
+  showListingModal(listing) {
+    //console.log(listing);
+    this.setState({
+      showModal: true,
+      selectedListing: listing
+    });
+  }
+
+  hideListingModal = () => {
+    this.setState({
+      showModal: false,
+      selectedListing: null
+    })
+  }
   render() {
+    var { listing, showModal, selectedListing } = this.state;
+    //console.log('what is passing to workouts from dashboard', this.props);
     return (
-      <Card.Group itemsPerRow={3}>
-        {this.props.data.map(listing => (
-          <Card key={listing.id}>
-            <Card.Content>
-              <Image src={this.props.user} size='mini' floated='left'/>
-              <Card.Header>{listing.title}</Card.Header>
-              <Card.Meta>{listing.location}</Card.Meta>
-              <Card.Description>{`${listing.details} on ${listing.date} for ${listing.duration} hour(s)`}</Card.Description>
-              <Card.Content extra>
-                <WorkoutDropdown postingId={listing.id} buddies={listing.buddies} update={this.props.update} dataPull={this.props.dataPull} />
-              </Card.Content>
-            </Card.Content>
-          </Card>
-        ))}
-      </Card.Group>
+      [<Transition visible={this.state.visible} duration={1000} animation='fade'>
+        <Container style={{marginTop: '20px'}}>
+          <Card.Group itemsPerRow={3}>
+            {this.props.data.map(listing => (
+              <Card key={listing.id} onClick={() =>this.showListingModal(listing)}>
+                <Card.Content>
+                  <Image src={this.props.userPic} size='mini' floated='left'/>
+                  <Card.Header>{listing.title}</Card.Header>
+                  <Card.Meta>{listing.location}</Card.Meta>
+                  <Card.Description>{`${listing.details} on ${listing.date} for ${listing.duration} hour(s)`}</Card.Description>
+                  <Card.Content extra>
+                    <WorkoutDropdown postingId={listing.id} buddies={listing.buddies} update={this.props.update} dataPull={this.props.dataPull} />
+                  </Card.Content>
+                </Card.Content>
+              </Card>
+            ))}
+          </Card.Group>
+        </Container>
+      </Transition>,
+      <Container>
+        {this.state.showModal && (
+          <ListingModal listing={selectedListing} open={this.state.showModal} 
+                        hideListingModal={this.hideListingModal} 
+                        user={this.props.user}
+                        userImage={this.props.userPic} />
+        )}
+      </Container>]
     )
   }
 
