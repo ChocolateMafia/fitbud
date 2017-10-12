060e3497271b39d330b182fe13c4beba1d4a24f2
Kenny Cao <kennyxcao@gmail.com>
2017-10-12 13:52:52 -0700

    Implement Profile picture from facebook or default sematic UI

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import { Container, Image, List } from 'semantic-ui-react';
 
 class Profile extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+
+    };
+  }  
 
   render() {
     return (
       <Container style={{margin: '30px'}}>
 
-        <Image src={this.props.user} size='small' shape='circular' centered style={{margin: 'auto'}} />
+        <Image src={this.props.user.picture} size='small' shape='circular' centered style={{margin: 'auto'}} />
       
-        <Container style={{"textAlign": "center"}}>
+        <Container style={{'textAlign': 'center'}}>
           <List style={{margin: '10px'}}>
             <List.Item>
-              <List.Header>My Dashboard</List.Header>
+              <List.Header>My Profile</List.Header>
             </List.Item>
           </List>
         </Container>
--- client/src/components/ProfilePic.js
+++ client/src/components/ProfilePic.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import { Container, Image, List } from 'semantic-ui-react';
 
 class ProfilePic extends Component {
-
   render() {
     return (
       <Container style={{margin: '30px'}}>
 
-        <Image src={this.props.user} size='small' shape='circular' centered style={{margin: 'auto'}} />
+        <Image src={this.props.user.picture} size='small' shape='circular' centered style={{margin: 'auto'}} />
       
-        <Container style={{"textAlign": "center"}}>
+        <Container style={{'textAlign': 'center'}}>
           <List style={{margin: '10px'}}>
             <List.Item>
               <List.Header>My Dashboard</List.Header>
