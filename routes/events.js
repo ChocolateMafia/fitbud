4c787916f286ae611befc1fdfce3c92fbbf687a7
Elena Bartosh <elena.karnaukhova@gmail.com>
2017-10-12 20:57:58 -0700

    Update events count

--- routes/events.js
+++ routes/events.js
@@ -4,12 +4,24 @@ var db = require('../database/index.js');
 
 router.get('/', (req, res) => {
   var id = req.user ? req.user.id : null;
-  console.log('id is ' + id);
 
   db.getEvents(id, (error, result) => {
     if (error) {
       console.error(error);
       res.status(200).json([{}]);
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+router.get('/count', (req, res) => {
+  var id = req.user ? req.user.id : null;
+
+  db.getEventsCount(id, (error, result) => {
+    if (error) {
+      console.error(error);
+      res.status(200).json(0);
     } else {
       res.status(200).json(result);
     }
