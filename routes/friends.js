c4e4d5d80996484680289196681cad115cd54fbb
Kenny Cao <kennyxcao@gmail.com>
2017-10-14 01:10:02 -0700

    Implement database fetch and filter for friends id and requester ids

--- routes/friends.js
+++ routes/friends.js
@@ -3,8 +3,29 @@ var router = express.Router();
 var db = require('../database/index.js');
 
 router.get('/:userId', (req, res) => {
+  var userId = +req.params.userId;
+  db.getUserFriendships(userId, (err, results) => {
+    var friends = results.filter(record => record.status === 'accept').map(record => {
+      if (record.userId === userId) {
+        return record.friendId;
+      }
+      if (record.friendId === userId) {
+        return record.userId;
+      }
+    }).filter((id, index, array) => array.indexOf(id) === index);
 
+    var requesters = results.filter(record => record.status === 'pending').reduce((arr, record) => {
+      if (record.friendId === userId) {
+        arr.push(record.userId);
+      }
+      return arr;
+    }, []);
 
+    console.log('*********** friends: ', friends);
+    console.log('*********** requesters: ', requesters);
+
+    err ? res.status(400).send() : res.json({friends, requesters});
+  });
   
   // db.findById(req.params.ownerId, (err, user) => {
   //   if (user.id === req.user) {
